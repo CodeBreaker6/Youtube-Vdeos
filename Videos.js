769a0993d08b4a065f5c2486a413051779d84954
CodeBreaker6 <90083761+CodeBreaker6@users.noreply.github.com>
2023-04-16 15:03:24 +0300

    Create Videos.js

---
+++ Videos.js
@@ -0,0 +1,12 @@
+const videos = {
+  "video1": {
+    "name": " JavaScript Tutorial for Beginners: Learn JavaScript in 1 Hour",
+    "url": "https://youtu.be/W6NZfCO5SIk",
+    "src": "https://i.ytimg.com/vi/kqtD5dpn9C8/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD1iLVjsmE_gK2uZS887LgyThzx4Q"
+  },
+  "video2": {
+    "name": " Python for Beginners - Learn Python in 1 Hour ",
+    "url": "https://youtu.be/kqtD5dpn9C8",
+    "src": "https://i.ytimg.com/vi/W6NZfCO5SIk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDOBWmb1XE5qRd1xinBxZPfYfXoEg",
+  },
+}
